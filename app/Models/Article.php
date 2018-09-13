cc2b0dad7fc21289f34df9e4e08218747cf1988d
userzhangjinlong <825188432@qq.com>
2018-09-13 17:20:05 +0800

    创建文章模型和迁移表文件

---
+++ app/Models/Article.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    /**
+     * 定义表名
+     * 约定优于配置解析：当模型名称为article时配置默认表名为articles如果没有主动定义表名的话
+     *
+     * @var string
+     */
+//    protected $table = 'articles';
+}
